02ae591c73e1d5fe5a00850e4c35537e9df01514
Allan Briffa <allan.briffa@wandera.com>
2023-01-31 17:18:56 +0000

    Fix for package reassing error

--- manifests/client/params.pp
+++ manifests/client/params.pp
@@ -1,6 +1,6 @@
 # @api private
 class mongodb::client::params inherits mongodb::globals {
-  $package_ensure = pick($mongodb::globals::version, 'present')
+  $version = pick($mongodb::globals::version, 'present')
   $manage_package = pick($mongodb::globals::manage_package, $mongodb::globals::manage_package_repo, false)
   $use_percona = pick($mongodb::globals::use_percona, 'false')
   $version_percona = pick($mongodb::globals::version_percona, 'present')
@@ -8,10 +8,11 @@ class mongodb::client::params inherits mongodb::globals {
   if $manage_package {
     if $use_percona == false {
       $package_name = "mongodb-${mongodb::globals::edition}-shell"
+      $package_ensure = $version
     } else {
       $package_name = 'percona-server-mongodb-shell'
-      $mongover = split($package_ensure, '[.]')
-      $package_ensure = "${package_ensure}-${mongover[2]}.${facts['os']['distro']['codename']}"
+      $mongover = split($version, '[.]')
+      $package_ensure = "${version}-${mongover[2]}.${facts['os']['distro']['codename']}"
     }
   } else {
     $package_name = $facts['os']['family'] ? {
